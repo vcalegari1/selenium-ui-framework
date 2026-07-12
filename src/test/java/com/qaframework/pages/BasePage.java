0cb06a693de4ab4dec5df663cd5c451376347b05
Vinicius Calegari <vinicius.calegari94@gmail.com>
2026-07-11 22:22:35 -0600

    Add BasePage with common actions and LoginPage for Sauce Demo login screen

---
+++ src/test/java/com/qaframework/pages/BasePage.java
@@ -0,0 +1,50 @@
+package com.qaframework.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected WebElement waitForVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected void click(By locator) {
+        waitForClickable(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        WebElement element = waitForVisible(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisible(locator).getText();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        try {
+            return waitForVisible(locator).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/com/qaframework/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package com.qaframework.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    private final By usernameField = By.id("user-name");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.id("login-button");
+    private final By errorMessage = By.cssSelector("[data-test='error']");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String username, String password) {
+        type(usernameField, username);
+        type(passwordField, password);
+        click(loginButton);
+    }
+
+    public String getErrorMessage() {
+        return getText(errorMessage);
+    }
+
+    public boolean isErrorDisplayed() {
+        return isDisplayed(errorMessage);
+    }
+}
